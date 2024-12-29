7e4a8edc75fc526d54417e4d17ccc0a36d27c176
Suman Info Tech <Gajendrasuman868@gmail.com>
2024-12-29 13:32:26 +0530

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "MiniCraft",
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Analytics/>
         {children}
       </body>
     </html>
--- src/app/mini-projects/layout.tsx
+++ src/app/mini-projects/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react"
+
 export const metadata: Metadata = {
   title: "Mini Projects",
   description: "MiniCraft is my personal portfolio platform designed to showcase my collection of mini projects. It serves as a digital space where I can organize, display, and reflect on the creative journey of my development work.",
@@ -12,7 +14,8 @@ export default function Main({
 }>) {
   return (
       <html lang="en">
-        <body>
+      <body>
+          <Analytics />
           {children}
         </body>
       </html>
