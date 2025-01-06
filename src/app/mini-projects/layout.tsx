d951a426081ba710e3b70fb696412db423eddceb
Suman Info Tech <Gajendrasuman868@gmail.com>
2025-01-06 15:25:24 +0530

    updated SEO

--- src/app/mini-projects/layout.tsx
+++ src/app/mini-projects/layout.tsx
@@ -4,8 +4,34 @@ import { Analytics } from "@vercel/analytics/react"
 import Navbar from "@/components/ui/global/navbar";
 
 export const metadata: Metadata = {
-  title: "Mini Projects",
-  description: "MiniCraft is my personal portfolio platform designed to showcase my collection of mini projects. It serves as a digital space where I can organize, display, and reflect on the creative journey of my development work.",
+  title: "MiniCraft | Mini Projects",
+  description: "MiniCraft is a curated gallery of creative mini projects that inspire and amaze. Explore the possibilities of innovation.",
+  keywords: "MiniCraft, mini projects, creative showcase, project gallery, tech innovation, portfolio platform",
+  authors: [{ name: "Gajender", url: "https://www.minicraft.com/" }],
+  viewport: "width=device-width, initial-scale=1.0",
+  openGraph: {
+    title: "MiniCraft",
+    description: "Discover inspiring mini projects crafted to showcase innovation and creativity. Explore now on MiniCraft!",
+    url: "https://www.minicraft.com/",
+    images: [
+      {
+        url: "/assets/logo/logo.png",
+        width: 1200,
+        height: 630,
+        alt: "MiniCraft Project Gallery",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "MiniCraft - Showcase Your Creativity",
+    description: "Explore an inspiring collection of mini projects that highlight creativity and innovation. Dive in on MiniCraft!",
+    images: ["/assets/logo/logo.png"],
+  },
+  icons: {
+    icon: "/assets/logo/logo.png"
+  }
 };
 
 export default function Main({
