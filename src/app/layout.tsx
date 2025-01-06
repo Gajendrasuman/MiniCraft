dfbda5a7dfb26058836d42a6dee2b0921b7057ce
Suman Info Tech <Gajendrasuman868@gmail.com>
2025-01-06 15:55:17 +0530

    Launch Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react"
 
-export const metadata:Metadata = {
+export const metadata: Metadata = {
   title: "MiniCraft",
   description: "MiniCraft is a curated gallery of creative mini projects that inspire and amaze. Explore the possibilities of innovation.",
   keywords: "MiniCraft, mini projects, creative showcase, project gallery, tech innovation, portfolio platform",
@@ -14,7 +14,7 @@ export const metadata:Metadata = {
     url: "https://www.minicraft.com/",
     images: [
       {
-        url: "/assets/logo/logo.png",
+        url: "/assets/images/og-image.png",
         width: 1200,
         height: 630,
         alt: "MiniCraft Project Gallery",
@@ -26,7 +26,7 @@ export const metadata:Metadata = {
     card: "summary_large_image",
     title: "MiniCraft - Showcase Your Creativity",
     description: "Explore an inspiring collection of mini projects that highlight creativity and innovation. Dive in on MiniCraft!",
-    images: ["/assets/logo/logo.png"],
+    images: ["/assets/images/og-image.png"],
   },
   icons: {
     icon: "/assets/logo/logo.png"
